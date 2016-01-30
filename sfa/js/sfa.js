ca2d930673e3d8da8f401df90ddad6c691b3b288
Henry <henryvo94@gmail.com>
2016-01-30 16:29:58 +1100

    Various QA changes - circle/form validation

--- sfa/js/sfa.js
+++ sfa/js/sfa.js
@@ -11,7 +11,6 @@ window.onload = function() {
 	$.each($squares, function(i,v) {
 		$(v).outerHeight($(v).outerWidth());
 	});
-
 	
 	initCarousel();
 	initAccordion();
@@ -58,7 +57,15 @@ var initAccordion = function() {
 
 		});
 
+		if ($(window).width() >= 800) {
+			$('.js-team-section-mob').appendTo('.team-details-div');
+
+			// Bounces the height for some reason
+			$('.js-team-section-mob').hide();
+		}
+
 		accordion.members.on('click touch', function(i,v) {
+
 
 			$(accordion).find('.active').removeClass('active');
 
@@ -74,12 +81,9 @@ var initAccordion = function() {
 			else {
 				$('.js-team-section-mob').slideUp();
 
-				if ($(window).width() >= 800) {
-					$('.js-team-section-mob').appendTo('.team-details-div');
-					$('.js-team-section-mob').hide();
-				}
 
-				else {
+
+				if ($(window).width() <= 800) {
 					$('html,body').animate({
 						scrollTop: $('.js-team-member.slide-' + target).offset().top
 					}, 500);
@@ -107,6 +111,36 @@ var initAccordion = function() {
 };
 
 
+// var disableSubmit = function() {
+// 	$('form > input').keyup(function() {
+// 		var empty = false;
+
+// 		$('form > input').each(function() {
+// 			if ($(this).val() == '') {
+// 				empty = true;
+// 			}
+// 		});
+
+// 		if (empty) {
+// 			$('.button').attr('disabled', 'disabled');
+// 		}
+// 		else {
+// 			$('.button').removeAttr('disabled');
+// 		}
+// 	})
+// }
+
+$('input').keyup(function() {
+	if ($('#firstName').val() != "" && $('#email').val() != "") {
+		// $('#submit').removeAttr('disabled');
+		$('#submit').removeClass('disableClick');
+	}
+	else {
+		// $('#submit').attr('disabled', true);
+		$('#submit').addClass('disableClick');
+	}
+})
+
 /* Sending data to Google Docs spreadsheets through ajax request */
 
 function postContactToGoogle() {
@@ -121,32 +155,62 @@ function postContactToGoogle() {
 
   // FIRST check to see if fields have a valid value. IF valid:
 
-  $.ajax({
-    url: "https://docs.google.com/a/pwc.com/forms/d/127C47i_BAUxGG7TMUW43CyN89dmmmjJl8kmLi73Khgs/formResponse",
-    data: { "entry.578566935": first, 
-    "entry.1652291931": last,
-    "entry_732368356": interests,
-    "entry.891975652": email },
-    type: "POST",
-    dataType: "xml",
-    traditional: true,
-    statusCode: {
-      0: function () {
-        
-        // CAN we make this a neater, in page message
-        alert("Your form has been submitted.");
+ //  if (email == null || email == "") {
+ //  	// alert("Please complete the form.");
+ //  	document.getElementById('email').style.borderColor = "red";
+ //  	// $('#email').after("TEST");
 
-        // will still get a CORS message for some reason
+ //  	if (first == null || first == "") {
+ //  	  	document.getElementById('firstName').style.borderColor = "red";
+	// }
+ //  }
 
-        $('form').find('input:text').val('');
-        $('input:checkbox').removeAttr('checked');
+ //  else if (first == null || first == "") {
+ //  	document.getElementById('firstName').style.borderColor = "red";
 
-      },
-      200: function () {
-        // console.log("error");
-      }
-    }
-  });
+ //  	if (email == null || email == "") {
+ //  		document.getElementById('email').style.borderColor = "red";
+ //  	}
+ //  }
+
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!re.test(email)) {
+		alert("Please put in valid email");
+		return false;
+	}
+
+  else {
+  	$.ajax({
+  	  url: "https://docs.google.com/a/pwc.com/forms/d/127C47i_BAUxGG7TMUW43CyN89dmmmjJl8kmLi73Khgs/formResponse",
+  	  data: { "entry.578566935": first, 
+  	  "entry.1652291931": last,
+  	  "entry_732368356": interests,
+  	  "entry.891975652": email },
+  	  type: "POST",
+  	  dataType: "xml",
+  	  traditional: true,
+  	  statusCode: {
+  	    0: function () {
+  	      
+  	      // CAN we make this a neater, in page message
+  	      alert("Your form has been submitted.");
+
+  	      // will still get a CORS message for some reason
+
+  	      $('form').find('input').val('');
+  	      $('input:checkbox').removeAttr('checked');
+  	      $('#submit').addClass('disableClick');
+
+  	    },
+  	    200: function () {
+  	      // console.log("error");
+  	    }
+  	  }
+  	});
+  }
+
+  
 
   // IF NOT VALID
 
