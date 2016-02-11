8567c2ef7ddee5d6a63ec362f276c79f054089db
Paul M Barnette <Paul M Barnette@au.aap.ad.pwcinternal.com>
2016-02-11 18:19:29 +1100

    Update to reintinalize the accordion and carousel

--- sfa/js/sfa.js
+++ sfa/js/sfa.js
@@ -1,115 +1,153 @@
 window.onload = function() {
 
-	var $btnsToggle = $('.js-btn-toggle');
+    var $btnsToggle = $('.js-btn-toggle'),
+	resizeTimer;
 
-	$btnsToggle.on('click touch', function(event) {
-		$('.' + event.target.dataset.modifies).toggleClass('active');
-	});
+    $btnsToggle.on('click touch', function(event) {
+        $('.' + event.target.dataset.modifies).toggleClass('active');
+    });
 
-	var $squares = $('.js-square');
+    var $squares = $('.js-square');
 
-	$.each($squares, function(i,v) {
-		$(v).outerHeight($(v).outerWidth());
-	});
-	
-	initCarousel();
-	initAccordion();
+    $.each($squares, function(i, v) {
+        $(v).outerHeight($(v).outerWidth());
+    });
 
-	$(window).bind('resize', function() {
-		initCarousel();
-		initAccordion();
-	});
+    initCarousel();
+    initAccordion();
+
+    $(window).bind('resize', function() {
+        
+		clearTimeout(resizeTimer);
+	  	resizeTimer = setTimeout(function() {
+	    	// Run code here, resizing has "stopped"
+		    //console.log('resize the window');
+	        initCarousel();
+	        reintinalizeAccordion();        
+	  	}, 250);
+        
+    });
 };
 
 // our scrolly navigation
 var scrollTo = function(elem, space) {
-	console.log(elem)
-	$('html,body').animate({
-		scrollTop: $(elem).offset().top - (60 + space)},
-		'slow');
-	if ($('.navigation-main').hasClass('active')) {
-		$('.navigation-main').removeClass('active')
-	}
+    console.log(elem)
+    $('html,body').animate({
+            scrollTop: $(elem).offset().top - (60 + space)
+        },
+        'slow');
+    if ($('.navigation-main').hasClass('active')) {
+        $('.navigation-main').removeClass('active')
+    }
 };
 
 
 // accordion content
 var initAccordion = function() {
 
-	var $accordions = $('.js-a-group');
+    var $accordions = $('.js-a-group');
 
-	$.each($accordions, function(i,accordion) {
+    $.each($accordions, function(i, accordion) {
 
-		// counter for position of the carousel
-		accordion.current = 0;
-		accordion.sections = $(accordion).find('.js-team-section-mob');
-		accordion.members = $(accordion).find('.js-team-member');
+    	console.log('start accordion');
+        // counter for position of the carousel
+        accordion.current = 0;
+        accordion.sections = $(accordion).find('.js-team-section-mob');
+        accordion.members = $(accordion).find('.js-team-member');
 
-		$.each(accordion.sections, function(ii, section) {
-			$(section).addClass('slide-' + ii);
-			section.dataset.modifier = (ii);
+        console.log('accordion %o', accordion);
+        $.each(accordion.sections, function(ii, section) {
+            $(section).addClass('slide-' + ii).data('slide',ii);
+            section.dataset.modifier = (ii);
 
-		});
+        });
 
-		$.each(accordion.members, function(ii, member) {
-			$(member).addClass('slide-' + ii);
-			member.dataset.modifies = (ii);
+        $.each(accordion.members, function(ii, member) {
+            $(member).addClass('slide-' + ii).data('slide',ii);
+            member.dataset.modifies = (ii);
 
-		});
+        });
 
-		if ($(window).width() >= 800) {
-			$('.js-team-section-mob').appendTo('.team-details-div');
+        reintinalizeAccordion();
 
-			// Bounces the height for some reason
-			$('.js-team-section-mob').hide();
-		}
-
-		accordion.members.on('click touch', function(i,v) {
+        accordion.members.on('click touch', function(i, v) {
 
 
-			$(accordion).find('.active').removeClass('active');
+            $(accordion).find('.active').removeClass('active');
 
-			var target = this.dataset.modifies;
+            var target = this.dataset.modifies;
 
-			$('.js-team-section-mob.active').slideUp();
+            $('.js-team-section-mob.active').slideUp();
 
-			if ( $('.js-team-section-mob.slide-' + target).is(':visible') ) {
-				$(accordion).find('.active').removeClass('active');
-				$('.js-team-section-mob').slideUp();
-			}
-
-			else {
-				$('.js-team-section-mob').slideUp();
+            if ($('.js-team-section-mob.slide-' + target).is(':visible')) {
+                $(accordion).find('.active').removeClass('active');
+                $('.js-team-section-mob').slideUp();
+            } else {
+                $('.js-team-section-mob').slideUp();
 
 
 
-				if ($(window).width() <= 800) {
-					$('html,body').animate({
-						scrollTop: $('.js-team-member.slide-' + target).offset().top
-					}, 500);
-				}
+                if ($(window).width() <= 800) {
+                    $('html,body').animate({
+                        scrollTop: $('.js-team-member.slide-' + target).offset().top
+                    }, 500);
+                }
 
-				$(accordion).find('.slide-' + target).addClass('active');
+                $(accordion).find('.slide-' + target).addClass('active');
 
-				$('.active').slideDown();
-			}
-		});
-	});
+                $('.active').slideDown();
+            }
+        });
+    });
 
 
-$accordions.on('click touch', function(i,v) {
-	
-	var target = ('mod-' + this.dataset.modifies);
-	var elem = $('.' + target);
-	if (elem.hasClass('open')) {
-		elem.removeClass('open').fadeOut();
-	} else {
-		$('.open').removeClass('open').fadeOut();
-		elem.fadeIn().addClass('open');
-	}
-});
+    $accordions.on('click touch', function(i, v) {
+
+        var target = ('mod-' + this.dataset.modifies);
+        var elem = $('.' + target);
+        if (elem.hasClass('open')) {
+            elem.removeClass('open').fadeOut();
+        } else {
+            $('.open').removeClass('open').fadeOut();
+            elem.fadeIn().addClass('open');
+        }
+    });
+    //console.log('accordion reintinalized');
+
+
+    $accordions.data('window-width', $(window).width());
+
 };
 
+
+var reintinalizeAccordion = function() {
+	var $accordions = $('.js-a-group'),
+	accordionWidth = $accordions.data('window-width');
+
+	if ($(window).width() >= 800 && accordionWidth < 800) {
+        //console.log('the with is greater than 800');
+        $('.js-team-section-mob').appendTo('.team-details-div');
+
+            // Changing the dom causes a reflow
+         $('.js-team-section-mob').hide();
+    }
+    else {
+        //console.log('need to reset to appendTo a different area');
+        $accordions.find('.js-team-section-mob').each(function(num){
+        	//console.log("%d: %o classes %o", num, $(this).data('slide'), 
+        	//	$(this).attr("class").split(' ') );
+        	//check to see if i need to move it if it isnt nested, need to move
+        	//console.log('find parent slide? %o len %o', $(this).parent('slide-'+$(this).data('slide')),
+        	//	$(this).parent('slide-'+$(this).data('slide')).length);
+        	if($(this).parent('slide-'+$(this).data('slide')).length === 0 ){
+        		$accordions.find('.js-team-member.slide-'+$(this).data('slide'))
+        		.append($(this));
+        	}
+        	//$accordions.find('.js-team-member.slide-'+$(this).data('slide')).
+        });
+    }
+    $accordions.data('window-width', $(window).width());
+};
 
 // var disableSubmit = function() {
 // 	$('form > input').keyup(function() {
@@ -131,205 +169,207 @@ $accordions.on('click touch', function(i,v) {
 // }
 
 $('input').keyup(function() {
-	if ($('#firstName').val() != "" && $('#email').val() != "") {
-		// $('#submit').removeAttr('disabled');
-		$('#submit').removeClass('disableClick');
-	}
-	else {
-		// $('#submit').attr('disabled', true);
-		$('#submit').addClass('disableClick');
-	}
+    if ($('#firstName').val() != "" && $('#email').val() != "") {
+        // $('#submit').removeAttr('disabled');
+        $('#submit').removeClass('disableClick');
+    } else {
+        // $('#submit').attr('disabled', true);
+        $('#submit').addClass('disableClick');
+    }
 })
 
 /* Sending data to Google Docs spreadsheets through ajax request */
 
 function postContactToGoogle() {
 
-	var first = $('#firstName').val();
-	var last = $('#lastName').val();
-	var email = $('#email').val();
+    var first = $('#firstName').val();
+    var last = $('#lastName').val();
+    var email = $('#email').val();
 
-	var interests = $('.interests:checked').map(function() {
-		return this.value;
-	}).get().join(", ");
+    var interests = $('.interests:checked').map(function() {
+        return this.value;
+    }).get().join(", ");
 
-  // FIRST check to see if fields have a valid value. IF valid:
+    // FIRST check to see if fields have a valid value. IF valid:
 
-  // Check to see if email is valid -> regex; also check if no checkboxes have been selected
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Check to see if email is valid -> regex; also check if no checkboxes have been selected
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (!re.test(email)) {
-  	alert("Please put in a valid email address");
-  	return false;
-  }
+    if (!re.test(email)) {
+        alert("Please put in a valid email address");
+        return false;
+    } else if (interests == null || interests == "") {
+        alert("Please tick at least one area of interest.")
+        return false;
+    } else {
+        $.ajax({
+            url: "https://docs.google.com/a/pwc.com/forms/d/127C47i_BAUxGG7TMUW43CyN89dmmmjJl8kmLi73Khgs/formResponse",
+            data: {
+                "entry.578566935": first,
+                "entry.1652291931": last,
+                "entry_732368356": interests,
+                "entry.891975652": email
+            },
+            type: "POST",
+            dataType: "xml",
+            traditional: true,
+            statusCode: {
+                0: function() {
 
-  else if (interests == null || interests == "") {
-  	alert("Please tick at least one area of interest.")
-  	return false;
-  }
+                    // CAN we make this a neater, in page message
+                    alert("Your form has been submitted.");
 
-  else {
-  	$.ajax({
-  		url: "https://docs.google.com/a/pwc.com/forms/d/127C47i_BAUxGG7TMUW43CyN89dmmmjJl8kmLi73Khgs/formResponse",
-  		data: { "entry.578566935": first, 
-  		"entry.1652291931": last,
-  		"entry_732368356": interests,
-  		"entry.891975652": email },
-  		type: "POST",
-  		dataType: "xml",
-  		traditional: true,
-  		statusCode: {
-  			0: function () {
-  				
-  	      // CAN we make this a neater, in page message
-  	      alert("Your form has been submitted.");
+                    // will still get a CORS message for some reason
 
-  	      // will still get a CORS message for some reason
+                    $('form').find('input').val('');
+                    $('input:checkbox').removeAttr('checked');
+                    $('#submit').addClass('disableClick');
 
-  	      $('form').find('input').val('');
-  	      $('input:checkbox').removeAttr('checked');
-  	      $('#submit').addClass('disableClick');
+                },
+                200: function() {
+                    // console.log("error");
+                }
+            }
+        });
+    }
 
-  	  },
-  	  200: function () {
-  	      // console.log("error");
-  	  }
-  	}
-  });
-  }
 
-  
 
-  // IF NOT VALID
+    // IF NOT VALID
 
-  // show message with form validation errors
-  
+    // show message with form validation errors
+
 };
 
 $('#down-arrow').click(function() {
-	$('html,body').animate({
-		scrollTop: $('#industries').offset().top
-	}, 800);
+    $('html,body').animate({
+        scrollTop: $('#industries').offset().top
+    }, 800);
 });
 
 $('#subscribe-button').click(function() {
-	$('html,body').animate({
-		scrollTop: $('#subscribe').offset().top
-	}, 800);
+    $('html,body').animate({
+        scrollTop: $('#subscribe').offset().top
+    }, 800);
 });
 
 var carouselClick = function(carousel) {
-	$.each(carousel.slides, function(ii,slide) {
-		if (slide.dataset.modifier > carousel.current) {
-			$(slide).removeClass('left active').addClass('right');
-		} else if (slide.dataset.modifier < carousel.current) {
-			$(slide).removeClass('active right').addClass('left');
-		} else {
-			$(slide).removeClass('left right').addClass('active');
-		}
-	});
+    $.each(carousel.slides, function(ii, slide) {
+        if (slide.dataset.modifier > carousel.current) {
+            $(slide).removeClass('left active').addClass('right');
+        } else if (slide.dataset.modifier < carousel.current) {
+            $(slide).removeClass('active right').addClass('left');
+        } else {
+            $(slide).removeClass('left right').addClass('active');
+        }
+    });
 
-	$.each(carousel.controls, function(ii,control) {
-		if (control.dataset.modifies != carousel.current) {
-			$(control).removeClass('active');
-		} else {
-			$(control).addClass('active');
+    $.each(carousel.controls, function(ii, control) {
+        if (control.dataset.modifies != carousel.current) {
+            $(control).removeClass('active');
+        } else {
+            $(control).addClass('active');
 
-			if ($(window).width() >= 800) {
-				// do nothing
-			}
-			else {
-				$('html,body').animate({
-					scrollTop: $('.control.slide-' + ii).offset().top
-				}, 500);
-			}
-		}
-	});
+            if ($(window).width() >= 800) {
+                // do nothing
+            } else {
+                $('html,body').animate({
+                    scrollTop: $('.control.slide-' + ii).offset().top
+                }, 500);
+            }
+        }
+    });
 };
 
 var carouselDelay = function(carousel) {
 
-	if (carousel.hover === false) {
+    if (carousel.hover === false) {
 
-		console.log('tick ' + carousel.current);
-		if (carousel.current < carousel.slides.length - 1) {
-			carousel.current++;
-		} else {
-			carousel.current = 0
-		}
-		carouselClick(carousel);
-	}
+        console.log('tick ' + carousel.current);
+        if (carousel.current < carousel.slides.length - 1) {
+            carousel.current++;
+        } else {
+            carousel.current = 0
+        }
+        carouselClick(carousel);
+    }
 };
 
 var initCarousel = function() {
 
-	// find all carousels
-	var $carousels = $('.js-carousel-container');
+    // find all carousels
+    var $carousels = $('.js-carousel-container');
 
-	// split each carousel and deal with it separately as 'carousel'
-	$.each($carousels, function(i,carousel) {
+    // split each carousel and deal with it separately as 'carousel'
+    $.each($carousels, function(i, carousel) {
 
-		// set up the carousel instance
+        // set up the carousel instance
 
-		// get the width of the container for later use
-		carousel.width = $(carousel).innerWidth();
-		// set the height of the container but we'll mess with it later
-		carousel.height = 0
-		// counter for position of the carousel
-		carousel.current = 0;
-		// find all the slides int he carousel
-		carousel.slides = $(carousel).find('.js-carousel-section');
-		// find all the controls 
-		carousel.controls = $(carousel).find('.js-carousel-control .control');
-		// monitor the hover state on the carousel
-		carousel.hover = false;
+        // get the width of the container for later use
+        carousel.width = $(carousel).innerWidth();
+        // set the height of the container but we'll mess with it later
+        carousel.height = 0
+            // counter for position of the carousel
+        carousel.current = 0;
+        // find all the slides int he carousel
+        carousel.slides = $(carousel).find('.js-carousel-section');
+        // find all the controls 
+        carousel.controls = $(carousel).find('.js-carousel-control .control');
+        // monitor the hover state on the carousel
+        carousel.hover = false;
 
-		// iterate over each slide in carousel and assign it a number, initial state and correct width
-		$.each(carousel.slides, function(ii,slide) {
-			$(slide).addClass('slide-' + ii);
-			slide.dataset.modifier = (ii);
-			$(slide).innerWidth(carousel.width);
-			// console.log(carousel.height)
-			// console.log(slide)
+        // iterate over each slide in carousel and assign it a number, initial state and correct width
+        $.each(carousel.slides, function(ii, slide) {
+            $(slide).addClass('slide-' + ii);
+            slide.dataset.modifier = (ii);
+            $(slide).innerWidth(carousel.width);
+            // console.log(carousel.height)
+            // console.log(slide)
 
-			if ( $(slide).innerHeight() > carousel.height ) {
-				carousel.height = $(slide).innerHeight();
+            if ($(slide).innerHeight() > carousel.height) {
+                carousel.height = $(slide).innerHeight();
 
-			}
-		});
+            }
+        });
 
-		// iterate over the controels and assign them a class also
-		$.each(carousel.controls, function(ii,control) {
-			$(control).addClass('slide-' + ii);
-			control.dataset.modifies = (ii);	
-		});
+        // iterate over the controels and assign them a class also
+        $.each(carousel.controls, function(ii, control) {
+            $(control).addClass('slide-' + ii);
+            control.dataset.modifies = (ii);
+        });
 
-		$(carousel.controls).on('click hover', function(event) {
-			carousel.current = this.dataset.modifies;
-			carouselClick(carousel);
-		});
+        $(carousel.controls).on('click hover', function(event) {
+            carousel.current = this.dataset.modifies;
+            carouselClick(carousel);
+        });
 
-		// Commented out height because messing with images
+        // Commented out height because messing with images
 
-		// iterate again to add the same height to all elements
-		
-		if ($(window).width() >= 800) {
-			$.each(carousel.slides, function(ii,slide) {
-				$(slide).innerHeight(carousel.height);
-				$(carousel).innerHeight(carousel.height);
-			});
-		}
-		
-		// monitor the hovering of the carousel - stop the animation if hovered
-		// $(carousel).hover(function() {
-		//   carousel.hover = true;
-		// }, function() {
-		//   carousel.hover = false;
-		// });
+        // iterate again to add the same height to all elements
 
-if ($(window).width() >= 800) {
-	carouselClick(carousel);
-}
+        if ($(window).width() >= 800) {
+            $.each(carousel.slides, function(ii, slide) {
+                $(slide).innerHeight(carousel.height);
+                $(carousel).innerHeight(carousel.height);
+            });
+        }
+        else {
+        	$.each(carousel.slides, function(ii, slide) {
+                $(slide).innerHeight('');
+                $(carousel).innerHeight('');
+            });	
+        }
 
-});
+        // monitor the hovering of the carousel - stop the animation if hovered
+        // $(carousel).hover(function() {
+        //   carousel.hover = true;
+        // }, function() {
+        //   carousel.hover = false;
+        // });
+
+        if ($(window).width() >= 800) {
+            carouselClick(carousel);
+        }
+		console.log('carousel reintinalized');
+    });
 };
